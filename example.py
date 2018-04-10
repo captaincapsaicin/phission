21311bd8200ac00914fc4b507d0382ff4b944636
Neil Thomas <thomas.a.neil@gmail.com>
2018-04-10 12:04:33 -0700

    Use only half the haplotypes to phase. Add a histogram of abs(output_values) to example output

--- example.py
+++ example.py
@@ -10,9 +10,9 @@ from switch_error import switch_error
 
 def main(num_snps):
     # simulate with msprime
-    all_haplotypes = simulate_haplotype_matrix(100)
+    all_haplotypes = simulate_haplotype_matrix(200)
     while all_haplotypes.shape[1] < num_snps:
-        all_haplotypes = simulate_haplotype_matrix(100)
+        all_haplotypes = simulate_haplotype_matrix(200)
     haplotypes = all_haplotypes[:, 0:num_snps]
 
     print('haplotype dimension')
@@ -21,6 +21,10 @@ def main(num_snps):
     genotypes = compress_to_genotype_matrix(haplotypes)
     unphased_haplotypes = get_incomplete_phasing_matrix(genotypes)
 
+
+    # what if we just used half of them
+    unphased_haplotypes = unphased_haplotypes[0::2]
+
     # this is what the ".phase" method does
     mask = get_mask(unphased_haplotypes)
     phased_haplotypes = nuclear_norm_solve(unphased_haplotypes, mask, mu=1)
@@ -28,15 +32,27 @@ def main(num_snps):
 
     # converting 0/1 representation to -1/1
     true_haplotypes = -1*(haplotypes == 0).astype(int) + 1*(haplotypes == 1).astype(int)
+
+    # just using half
+    true_haplotypes = true_haplotypes[0::2]
+
     # maybe we'll use something like thresholding instead of rounding
-    thresh = -1*(phased_haplotypes < 0) + 1*(phased_haplotypes > 0)
+    # thresh = -1*(phased_haplotypes < 0) + 1*(phased_haplotypes > 0)
 
     headers = ['nuclear norm', 'rank']
     data = [['true', np.linalg.norm(true_haplotypes, 'nuc'), np.linalg.matrix_rank(true_haplotypes)],
             ['unphased', np.linalg.norm(unphased_haplotypes, 'nuc'), np.linalg.matrix_rank(unphased_haplotypes)],
             ['phased', np.linalg.norm(phased_haplotypes, 'nuc'), np.linalg.matrix_rank(phased_haplotypes)],
-            ['rounded', np.linalg.norm(rounded, 'nuc'), np.linalg.matrix_rank(rounded)],
-            ['thresholded', np.linalg.norm(thresh, 'nuc'), np.linalg.matrix_rank(thresh)]]
+            ['rounded', np.linalg.norm(rounded, 'nuc'), np.linalg.matrix_rank(rounded)]]
+    print(tabulate(data, headers=headers))
+
+    # histogram of absolute values
+    bins = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2])
+    headers = bins[1:]
+    data = [['true', *np.histogram(abs(true_haplotypes), bins=bins)[0]],
+            ['unphased', *np.histogram(abs(unphased_haplotypes), bins=bins)[0]],
+            ['phased', *np.histogram(abs(phased_haplotypes), bins=bins)[0]],
+            ['rounded', *np.histogram(abs(rounded), bins=bins)[0]]]
     print(tabulate(data, headers=headers))
 
     headers = ['0s phased', 'Nonzero set to 0s', '0s remaining']
