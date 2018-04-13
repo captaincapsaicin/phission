ea4fee605b0cf26fde4da7d45213a228a28ab865
Neil Thomas <thomas.a.neil@gmail.com>
2018-04-13 13:33:12 -0700

    Add reference individuals to phasing. Add argument that sets the number of reference individuals

--- example.py
+++ example.py
@@ -8,12 +8,17 @@ from nuclear_norm_minimization import get_mask, nuclear_norm_solve
 from switch_error import switch_error
 
 
-def main(num_snps, mu=2):
+def main(num_snps, mu=2, num_ref=0):
     # simulate with msprime
     all_haplotypes = simulate_haplotype_matrix(200)
     while all_haplotypes.shape[1] < num_snps:
         all_haplotypes = simulate_haplotype_matrix(200)
     haplotypes = all_haplotypes[:, 0:num_snps]
+
+    # converting 0/1 representation to -1/1
+    true_haplotypes = -1*(haplotypes == 0).astype(int) + 1*(haplotypes == 1).astype(int)
+    # just using half
+    # true_haplotypes = true_haplotypes[0::2]
 
     print('haplotype dimension')
     print(haplotypes.shape)
@@ -21,26 +26,26 @@ def main(num_snps, mu=2):
     genotypes = compress_to_genotype_matrix(haplotypes)
     unphased_haplotypes = get_incomplete_phasing_matrix(genotypes)
 
-
     # what if we just used half of them
     unphased_haplotypes = unphased_haplotypes[0::2]
+
+    # supplement with reference (true) haplotypes
+    print('{} reference haplotypes added'.format(num_ref))
+    unphased_haplotypes = np.vstack([unphased_haplotypes, true_haplotypes[0:num_ref]])
+    true_with_ref = np.vstack([true_haplotypes[0::2], true_haplotypes[0:num_ref]])
+    print('unphased dimension {}'.format(unphased_haplotypes.shape))
+    # unphased_haplotypes[0:num_ref] = true_haplotypes[0:num_ref]
 
     # this is what the ".phase" method does
     mask = get_mask(unphased_haplotypes)
     phased_haplotypes = nuclear_norm_solve(unphased_haplotypes, mask, mu=mu)
     rounded = np.matrix.round(phased_haplotypes)
 
-    # converting 0/1 representation to -1/1
-    true_haplotypes = -1*(haplotypes == 0).astype(int) + 1*(haplotypes == 1).astype(int)
-
-    # just using half
-    true_haplotypes = true_haplotypes[0::2]
-
     # maybe we'll use something like thresholding instead of rounding
     # thresh = -1*(phased_haplotypes < 0) + 1*(phased_haplotypes > 0)
 
     headers = ['nuclear norm', 'rank']
-    data = [['true', np.linalg.norm(true_haplotypes, 'nuc'), np.linalg.matrix_rank(true_haplotypes)],
+    data = [['true', np.linalg.norm(true_with_ref, 'nuc'), np.linalg.matrix_rank(true_with_ref)],
             ['unphased', np.linalg.norm(unphased_haplotypes, 'nuc'), np.linalg.matrix_rank(unphased_haplotypes)],
             ['phased', np.linalg.norm(phased_haplotypes, 'nuc'), np.linalg.matrix_rank(phased_haplotypes)],
             ['rounded', np.linalg.norm(rounded, 'nuc'), np.linalg.matrix_rank(rounded)]]
@@ -49,7 +54,7 @@ def main(num_snps, mu=2):
     # histogram of absolute values
     bins = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2])
     headers = bins[1:]
-    data = [['true', *np.histogram(abs(true_haplotypes), bins=bins)[0]],
+    data = [['true', *np.histogram(abs(true_with_ref), bins=bins)[0]],
             ['unphased', *np.histogram(abs(unphased_haplotypes), bins=bins)[0]],
             ['phased', *np.histogram(abs(phased_haplotypes), bins=bins)[0]],
             ['rounded', *np.histogram(abs(rounded), bins=bins)[0]]]
@@ -68,7 +73,8 @@ def main(num_snps, mu=2):
 if __name__ == '__main__':
     num_snps = int(sys.argv[1])
     mu = float(sys.argv[2])
-    main(num_snps, mu)
+    num_ref = int(sys.argv[3])
+    main(num_snps, mu, num_ref)
 
 # TODO nthomas: add switch error
 # we can't really mess with switch error until we've set all the 0s to nonzero values
