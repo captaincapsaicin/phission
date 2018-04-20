67a16cba25294f633194ff56a84aa795aea63edb
Neil Thomas <thomas.a.neil@gmail.com>
2018-04-20 12:57:09 -0700

    Add cleanup and randomness to run beagle. Suppress beagle std output. Compute switch error

--- run_beagle.py
+++ run_beagle.py
@@ -1,7 +1,9 @@
+import os
 import subprocess
 
 import msprime
 
+from switch_error import switch_error
 from utils import read_haplotype_matrix_from_vcf
 
 input_vcf = 'input.vcf'
@@ -10,29 +12,43 @@ output_file = 'beagle_output'
 beagle_path = 'beagle.27Jan18.7e1.jar'
 
 
-def main():
+def cleanup():
+    os.remove(input_vcf)
+    os.remove(output_file + '.vcf')
+
+
+def main(verbose=False, seed=None):
     tree_sequence = msprime.simulate(
          sample_size=100,
          Ne=1e5,
          length=5e3,
          recombination_rate=2e-8,
          mutation_rate=2e-8,
-         random_seed=10)
+         random_seed=seed)
 
     with open(input_vcf, 'w') as f:
         tree_sequence.write_vcf(f, ploidy=2)
 
-    subprocess.check_call('java -jar {} gt={} out={}'.format(beagle_path, input_vcf, output_file), shell=True)
+    # make it fully unphased replace | -> /
+    subprocess.check_call('sed -i.bak "s/|/\//g" {}'.format(input_vcf), shell=True)
+    # call beagle, suppress stdout
+    beagle_command = 'java -jar {} gt={} out={}'.format(beagle_path, input_vcf, output_file)
+    if not verbose:
+        beagle_command += ' >/dev/null'
+    subprocess.check_call(beagle_command, shell=True)
+
     beagle_output = output_file + '.vcf.gz'
     # probably don't need this and can read directly from gzipped .vcf.gz
     subprocess.check_call('gunzip {}'.format(beagle_output), shell=True)
     beagle_output_vcf = output_file + '.vcf'
 
-    haplotype_matrix = tree_sequence.genotype_matrix().T
-    recovered_haplotype_matrix = read_haplotype_matrix_from_vcf(beagle_output_vcf)
+    true_haplotypes = tree_sequence.genotype_matrix().T
+    phased_haplotypes = read_haplotype_matrix_from_vcf(beagle_output_vcf)
 
-
-
+    print('switch error')
+    print(switch_error(phased_haplotypes, true_haplotypes))
+    return true_haplotypes, phased_haplotypes
 
 if __name__ == '__main__':
     main()
+    cleanup()
